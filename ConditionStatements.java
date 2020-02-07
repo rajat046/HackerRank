a430f51514e903df391f4e0ec4647c27ed8b5ae5
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-02-07 21:10:31 +0530

    Update ConditionStatements.java

--- ConditionStatements.java
+++ ConditionStatements.java
@@ -8,9 +8,7 @@ If  is even and in the inclusive range of  to , print Not Weird
 If  is even and in the inclusive range of  to , print Weird
 If  is even and greater than , print Not Weird
 Complete the stub code provided in your editor to print whether or not  is weird.
-
 */
-
 
 import java.util.Scanner;
 
