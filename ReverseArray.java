f542cbcd772a4c8d8ea2fc2540a41ca5f4ba981a
Rajat Talreja <37415458+rajat046@users.noreply.github.com>
2020-02-24 11:54:09 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int[] A = new int[N];
+        for(int i = 0; i < N; i++)
+        {
+            A[i] = scan.nextInt();
+        }
+        for(int j = A.length - 1; j >= 0; j--)
+        {
+            System.out.print(A[j] + " ");
+        }
+    }
+}
